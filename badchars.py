c3c7d4dfc62297849dd89795741288147b0bbac5
Kyle Keefer <kwkeefer@gmail.com>
2022-10-30 13:46:29 -0700

    Behavior was inconsistent on windows and linux.  Fixing that.

--- badchars.py
+++ badchars.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import binascii
+import re
 
 app = Flask(__name__)
 
@@ -7,27 +8,34 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def main(path):
-    badchars = [f"\\{x}" for x in path.split("/")]
+    hex_chars = re.findall(r"x([a-fA-F0-9]{2})", path)
+    badchars = [f"{x}" for x in hex_chars]
 
-    return generate_chars(badchars)
+    chars = generate_chars(badchars)
+    print(chars)
+    return chars
 
 
 def generate_chars(badchars: [str]) -> bytes:
     """
     generate all chars after filtering out badchars
-    :param badchars: ["\\x0a", "\\x0d"]
+    :param badchars: ["0a", "0d"]
     :return: "0102.."
 
     Credit https://github.com/the-c0d3r/buffer-overflow/
     """
     char_str = ""
+    print(badchars)
 
     for x in range(1, 256):
-        current_char = "\\x" + '{:02x}'.format(x)
+        current_char = '{:02x}'.format(x)
 
         if current_char not in badchars:
-            char_str += current_char[2:]
-            # remove the leading "\x"
+            char_str += current_char
+
+        else:
+            print(current_char)
+
     return binascii.unhexlify(char_str)
 
 
